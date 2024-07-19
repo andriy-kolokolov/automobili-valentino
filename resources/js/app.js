19796eb72dda233830c18e2157d10d46e200f380
andriy-kolokolov <koloandre22@gmail.com>
2024-07-19 17:45:02 +0200

    done initial setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,8 +9,8 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    title: (title) => `${ title } - ${ appName }`,
+    resolve: (name) => resolvePageComponent(`./Pages/${ name }.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
@@ -20,4 +20,6 @@ createInertiaApp({
     progress: {
         color: '#4B5563',
     },
+}).then(() => {
+    console.log('Inertia app created!');
 });
